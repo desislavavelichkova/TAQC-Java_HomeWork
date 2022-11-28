d6da8d4caae500466a1181c3d3a36b2f1d7a4fa0
dashov <demdashuk@gmail.com>
2022-11-28 23:19:40 +0200

    Added code to file

---
+++ Task2/task2.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+        System.out.print("Enter number: ");
+        String numberString = System.console().readLine();
+        Integer number;
+
+        try {
+            number = Integer.parseInt(numberString);
+        } catch (NumberFormatException e) {
+            System.out.println("Entered string is not a valid number");
+            return;
+        }
+
+        if (number < 0) {
+            System.out.println("negative " + (Integer.toString(number).length() - 1) + "-digit");
+        } else {
+            System.out.println("positive " + Integer.toString(number).length() + "-digit");
+        }
+    }
