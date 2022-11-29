33e317bebceb5f4a919304b795e52d1311430bca
dashov <demdashuk@gmail.com>
2022-11-29 18:05:04 +0200

    Created Task_3.java and Task_4.java

--- dashov/Task3/Task_3.java
+++ dashov/Task3/Task_3.java
@@ -1,4 +1,41 @@
-package dashov.Task3;
+class Task_3 {
+    static void find_century(int year) {
+        if (year <= 0)
+            System.out.print("0 and negative is not allow"
+                    + "for a year");
+        else if (year <= 100)
+            System.out.print("1st century\n");
 
-public class Task_3 {
+        else if (year % 100 == 0)
+            System.out.print(year / 100 + " century\n");
+
+        else
+            System.out.print(year / 100 + 1 + " century\n");
+    }
+
+    static void is_year_intercalary(int year){
+        boolean leap;
+
+        if (year % 4 == 0) {
+            leap = true;
+
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+        } else leap = false;
+        if (leap)
+            System.out.println(year + " is a intercalary year.\n");
+        else
+            System.out.println(year + " is not a intercalary year.\n");
+    }
+
+    public static void main(String[] args) {
+        int year = 1996;
+        find_century(year);
+        is_year_intercalary(year);
+
+    }
 }
